a9b0a8476f256cdd8bda585b4e48f9963fa61b7a
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-05-12 16:49:42 +0100

    ex00 : create classes 🎈

---
+++ ex00/ScalarConverter.cpp
@@ -0,0 +1,21 @@
+#include "ScalarConverter.hpp"
+
+ScalarConverter::ScalarConverter() {
+    // Constructor implementation
+}
+
+ScalarConverter::~ScalarConverter() {
+    // Destructor implementation
+}
+
+ScalarConverter::ScalarConverter(const ScalarConverter& other) {
+    // Copy constructor implementation
+}
+
+ScalarConverter& ScalarConverter::operator=(const ScalarConverter& other) {
+    // Copy assignment operator implementation
+    if (this != &other) {
+        // Perform deep copy
+    }
+    return *this;
+}
---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,16 @@
+#ifndef SCALARCONVERTER_HPP
+#define SCALARCONVERTER_HPP
+
+class ScalarConverter {
+public:
+    ScalarConverter();
+    ~ScalarConverter();
+
+    ScalarConverter(const ScalarConverter& other);
+    ScalarConverter& operator=(const ScalarConverter& other);
+
+private:
+    // Add member variables here
+};
+
+#endif // SCALARCONVERTER_HPP
