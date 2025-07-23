f1a4f4cea0141065a658645e816173a853937fa1
ohssineabdelfatah <abdeoh304@gmail.com>
2025-07-24 00:55:49 +0100

    ex00 : pretty done

---
+++ cpp-08/ex00/Easyfind.hpp
@@ -0,0 +1,16 @@
+#ifndef EASYFIND_HPP
+#define EASYFIND_HPP
+
+#define ERROR 1
+
+template <typename T>
+
+int easyfind(T &data, int target)
+{
+    if(std::find(data.begin(), data.end(), target) == data.end())
+        throw std::exception();
+    return *(std::find(data.begin(), data.end(), target));
+}
+
+
+#endif // EASYFIND_HPP
---
+++ cpp-08/ex00/main.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+# include <algorithm>
+# include <vector>
+# include <list>
+#include"Easyfind.hpp"
+
+int main()
+{
+    std::vector<int> c1;
+    for(int i = 0; i < 10 ; i++)
+        c1.push_back(i*3);
+    for (
+    std::vector<int>::iterator it = c1.begin();
+    it != c1.end();
+    ++it){
+        std::cout << "list "<< *it << std::endl;
+    }
+    try{
+        std::cout << easyfind(c1, 2) << std::endl;
+    }catch(std::exception &e){
+        std::cerr << e.what() << " value not found "<< std::endl;
+    }
+    
+    try{
+        std::cout  << easyfind(c1, 0) << std::endl;
+    }catch(std::exception &e){
+        std::cerr << e.what() << " value not found "<< std::endl;
+    }
+
+    std::list<int> myList;
+    myList.push_back(10);
+    myList.push_back(20);
+    myList.push_back(30);
+    myList.push_back(40);
+
+    for (
+        std::list<int>::iterator it = myList.begin();
+        it != myList.end();
+        ++it){
+            std::cout << "list "<< *it << std::endl;
+        }
+    try{
+        std::cout << easyfind(myList, 2) << std::endl;
+    }catch(std::exception &e){
+        std::cerr << e.what() << " value not found "<< std::endl;
+    }
+    
+    try{
+        std::cout << easyfind(myList, 10) << std::endl;
+    }catch(std::exception &e){
+        std::cerr << e.what() << " value not found "<< std::endl;
+    }
+} 
