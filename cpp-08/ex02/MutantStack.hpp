bbdb0ff5f86ebe8fb62e104c4667e164a5c3b872
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-08-02 19:32:14 +0100

    cpp08 : added ex02 , not finished yet

---
+++ cpp-08/ex02/MutantStack.hpp
@@ -0,0 +1,16 @@
+#ifndef MUTANTSTACK_HPP
+#define MUTANTSTACK_HPP
+
+class MutantStack {
+public:
+    MutantStack();
+    ~MutantStack();
+
+    MutantStack(const MutantStack& other);
+    MutantStack& operator=(const MutantStack& other);
+
+private:
+    // Add member variables here
+};
+
+#endif // MUTANTSTACK_HPP
