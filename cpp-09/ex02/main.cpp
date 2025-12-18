061a6c2fa1763c8fc29c99a7da526635e53ee21f
OhssineAbdelfatah <abdelfatah.ohssine@gmail.com>
2025-12-18 02:01:01 +0100

    cpp9 ex02 : added timing

--- cpp-09/ex02/main.cpp
+++ cpp-09/ex02/main.cpp
@@ -1,5 +1,6 @@
 #include"PmergeMe.hpp"
-
+#include <ctime>
+#include <iomanip>
 int main(int ac , char *av[])
 {
     if(ac  >=  2 ){
@@ -15,20 +16,27 @@ int main(int ac , char *av[])
             timeVec = clock();
             PmergeMe vect;
             vect.MergeInsertVect(av + 1, ac);
-            timeVec = - clock() - timeVec ;
+            timeVec = clock() - timeVec ;
 
             //Container 2
             timeDeq = clock();
             vect.MergeInsertDque(av+1, ac);
-            timeDeq = - clock() - timeDeq ;
+            timeDeq = clock() - timeDeq ;
 
             std::cout << "before ... " ;
             vect.printVector(vect.getVectSeq());
             std::cout << "after ... " ;
             vect.printVector(vect.getVect());
 
-            std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector : " << (double)timeVec  << " ms" << std::endl;
-		    std::cout << "Time to process a range of " << ac - 1 << " elements with std::deque : " << (double)timeDeq  << " ms" << std::endl;
+	    double tvec = (double)timeVec / CLOCKS_PER_SEC;
+		double tdeq = (double)timeDeq / CLOCKS_PER_SEC;
+	    
+//		std::cout << std::fixed << std::setprecision(8) << tvec << "s" << std::endl;
+//		std::cout << std::fixed << std::setprecision(8) << tdeq << "s" << timeDeq << std::endl;
+            std::cout << "Time to process a range of " << ac - 1 << " elements with std::vector : " ;
+            std::cout << std::fixed << std::setprecision(8) << tvec  << " s" << std::endl;
+            std::cout << "Time to process a range of " << ac - 1 << " elements with std::deque : " ;
+	    std::cout << std::fixed << std::setprecision(8)  << tdeq << " s" << std::endl;
 
         }catch (std::exception& e){
             std::cout << "" << e.what() << std::endl;
